80dec4fb3e7e9dd3b1d5a89442667d887939eabd
mbidnyj <mbidnyj@gmail.com>
2023-12-07 17:42:30 +0100

    Initial Commit

---
+++ AI Podcast/AI_PodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_PodcastApp.swift
+//  AI Podcast
+//
+//  Created by Maksym Bidnyi on 07.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AI_PodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Podcast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Podcast
+//
+//  Created by Maksym Bidnyi on 07.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
