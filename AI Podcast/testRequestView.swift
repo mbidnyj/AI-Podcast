d33c2369d8eb0bedb956c7dacfe2e5bee799dd3d
mbidnyj <mbidnyj@gmail.com>
2023-12-10 18:50:00 +0100

    first commit

---
+++ AI Podcast/testRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  testRequestView.swift
+//  AI Podcast
+//
+//  Created by Maksym Bidnyi on 10.12.2023.
+//
+
+import SwiftUI
+
+struct testRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testRequestView()
+}
