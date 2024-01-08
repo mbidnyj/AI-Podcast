cd450b350d7216c90702bac3436fc425ed65080b
mbidnyj <mbidnyj@gmail.com>
2024-01-08 17:41:39 +0100

    pre-production version

--- AI Podcast/Utilities/Constants.swift
+++ AI Podcast/Utilities/Constants.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct Constants {
     // URLs, Keys, and any other constants
-    static let getAuth = "http://192.168.68.104:3000/api/getAuth"
-    static let createEpisode = "http://192.168.68.104:3000/api/createEpisode"
-    static let retrieveEpisode = "http://192.168.68.104:3000/api/retrieveEpisode"
-    static let getCover = "http://192.168.68.104:3000/api/getCover"
 //    static let getAuth = "http://localhost:3000/api/getAuth"
 //    static let createEpisode = "http://localhost:3000/api/createEpisode"
 //    static let retrieveEpisode = "http://localhost:3000/api/retrieveEpisode"
 //    static let getCover = "http://localhost:3000/api/getCover"
     
+    static let getAuth = "https://orca-app-ilspx.ondigitalocean.app/api/getAuth"
+    static let createEpisode = "https://orca-app-ilspx.ondigitalocean.app/api/createEpisode"
+    static let retrieveEpisode = "https://orca-app-ilspx.ondigitalocean.app/api/retrieveEpisode"
+    static let getCover = "https://orca-app-ilspx.ondigitalocean.app/api/getCover"
 }
